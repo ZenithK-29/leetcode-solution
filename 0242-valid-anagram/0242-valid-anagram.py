a9f4edb196ba9fd70c38f613ca104e4e7c943a6a
Zenith Kokkodan <zenithprof29@gmail.com>
2026-06-24 20:52:16 +0530

    Time: 27 ms (24.48%), Space: 12.4 MB (98.28%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        if len(s) == len(t):
+
+            if Counter(s) == Counter(t):
+                return True
+        
+        return False
+
+          
