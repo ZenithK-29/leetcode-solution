8e1b4812f9f6a8bd7c46652ecf95a4ebab5cbb8f
Zenith Kokkodan <zenithprof29@gmail.com>
2026-07-13 16:47:40 +0530

    Time: 0 ms (100%), Space: 22.1 MB (87.8%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,28 +6,51 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+
+
+        #iterative soln
+
+        n = 0
+        curr = root
+        stack = []
+
+        while curr or stack:
+
+            while curr: #add all leftmode nodes into stack for inorder traversal
+                stack.append(curr)
+                curr = curr.left
+            
+            curr = stack.pop()  #pop the topmost element of stack after we hit null
+
+            n+=1  #nth smallest element
+
+            if n == k: #if nth smalles  = kth smallest
+                return curr.val
+            
+            curr = curr.right #go to right node
         
-        count = [0]
-        def inorder_dfs(node):
+        #recursive soln
+        # count = [0]
+        # def inorder_dfs(node):
 
-            if not node:
-                return 
+        #     if not node:
+        #         return 
             
-            left = inorder_dfs(node.left)
+        #     left = inorder_dfs(node.left)
 
-            if left:
-                return left
+        #     if left:
+        #         return left
             
-            count[0] +=1
-            if count[0] == k:
-                return node
+        #     count[0] +=1
+        #     if count[0] == k:
+        #         return node
             
 
-            right = inorder_dfs(node.right)
+        #     right = inorder_dfs(node.right)
 
-            if right:
-                return right
+        #     if right:
+        #         return right
             
-            return None
+        #     return None
         
-        return inorder_dfs(root).val
+        # return inorder_dfs(root).val
