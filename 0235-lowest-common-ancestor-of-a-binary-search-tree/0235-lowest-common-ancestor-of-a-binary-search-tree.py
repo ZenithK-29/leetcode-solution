d578e96cdaa8da98cc29406fc1252ba217a0fe4f
Zenith Kokkodan <zenithprof29@gmail.com>
2026-07-18 23:28:15 +0530

    Time: 72 ms (17.77%), Space: 22.8 MB (17.67%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -8,19 +8,34 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        if not root:
-            return None
-        
-        if root == p or root == q:
-            return root
-        
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
+        #iterative soln
 
-        if left is not None and right is not None:
-            return root
-        
-        if left is not None and right is None:
-            return left
+        curr = root
 
-        return right
+        while curr:
+
+            if curr.val > p.val and curr.val > q.val:
+                curr = curr.left
+            
+            elif curr.val < p.val and curr.val < q.val:
+                curr = curr.right
+            else:
+                return curr
+
+        # #recursive soln
+        # if not root:
+        #     return None
+        
+        # if root == p or root == q:
+        #     return root
+        
+        # left = self.lowestCommonAncestor(root.left, p, q)
+        # right = self.lowestCommonAncestor(root.right, p, q)
+
+        # if left is not None and right is not None:
+        #     return root
+        
+        # if left is not None and right is None:
+        #     return left
+
+        # return right
