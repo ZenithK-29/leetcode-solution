458c58090718d2b35778ae4b1c4d966f33e5e4f5
Zenith Kokkodan <zenithprof29@gmail.com>
2026-07-22 21:56:20 +0530

    Time: 3 ms (32.6%), Space: 19.2 MB (91.46%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,18 +1,12 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isValid(self, s: str) -> bool:
         
-        if len(s) == 1:
-            return False
-
         mapping = {
-            ')':'(',
+            ')': '(',
             ']': '[',
             '}': '{'
         }
+
         stack = []
 
         for br in s:
@@ -20,12 +14,9 @@ class Solution(object):
             if br in mapping:
                 if not stack or stack[-1] != mapping[br]:
                     return False
-
                 stack.pop()
-
+            
             else:
                 stack.append(br)
         
         return len(stack) == 0
-
-
