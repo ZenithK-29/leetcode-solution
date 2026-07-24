fc5fa60494cb60ca33b5a39832a8d38bfa187411
Zenith Kokkodan <zenithprof29@gmail.com>
2026-07-24 08:09:44 +0530

    Time: 43 ms (33.85%), Space: 19.3 MB (52.9%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,20 +3,20 @@ class Solution:
         
         i = 0
         j = len(s1) - 1
-        count_s1 = Counter(s1)
-        count_s2 = Counter(s2[i:j+1])
+        count1 = Counter(s1)
+        count2 = Counter(s2[i:j+1])
 
         while j < len(s2):
 
-            if count_s1 == count_s2:
+            if count1 == count2:
                 return True
             
-            count_s2[s2[i]] -=1
+            count2[s2[i]] -=1
 
-            i+=1
+            i +=1
             j +=1
 
             if j < len(s2):
-                count_s2[s2[j]] +=1
+                count2[s2[j]] +=1
         
         return False
