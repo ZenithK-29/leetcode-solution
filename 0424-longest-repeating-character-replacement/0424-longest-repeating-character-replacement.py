256bbc04db19b993e1bde313e41d35dbe072719d
Zenith Kokkodan <zenithprof29@gmail.com>
2026-07-24 07:00:13 +0530

    Time: 491 ms (5.01%), Space: 15.3 MB (60.23%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -7,15 +7,16 @@ class Solution(object):
         """
         
         l = 0
-        longest = 0
         count = [0] * 26
+        longest = 0
 
         for r in range(len(s)):
+
             count[ord(s[r]) - 65] +=1
 
-            while (r-l+1) -  max(count) > k:
+            while (r-l+1) - max(count) > k:
                 count[ord(s[l]) - 65] -=1
-                l +=1
+                l+=1
 
             longest = max(r-l+1, longest)
 
