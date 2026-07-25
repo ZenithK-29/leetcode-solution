6a1e3bc2ac9ab5243e46660565b850cda0f0d448
Zenith Kokkodan <zenithprof29@gmail.com>
2026-07-25 12:43:45 +0530

    Time: 4 ms (69.57%), Space: 23 MB (41.89%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,16 +2,40 @@ import heapq
 from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
+
+        #Bucket Sort --> O(n) soln
+        n = len(nums)
+        bucket = [0] * (n+1)
         count = Counter(nums)
-        heap = []
 
-        for key, value in count.items():
+        #mapped the num to their respective indexes where the indexes matches the freq of the num
+        for num, freq in count.items():
 
-            if len(heap) < k:
-                heapq.heappush(heap, (value, key))
+            if bucket[freq] == 0:
+                bucket[freq] = [num]
             else:
-                heapq.heappushpop(heap, (value, key))
+                bucket[freq].append(num)
+        
+        res = []
+
+        for i in range(n, -1, -1):
+            if bucket[i] != 0:
+                res.extend(bucket[i])
+            if len(res) == k:
+                break
+        
+        return res
+        
+        #heap soln
+        # count = Counter(nums)
+        # heap = []
+
+        # for key, value in count.items():
+
+        #     if len(heap) < k:
+        #         heapq.heappush(heap, (value, key))
+        #     else:
+        #         heapq.heappushpop(heap, (value, key))
             
-        return [h[1] for h in heap]
+        # return [h[1] for h in heap]
         
