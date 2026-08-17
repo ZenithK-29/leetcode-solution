ac61bba1e15b3ca56ab499dc2a49d62ed2f60263
Zenith Kokkodan <zenithprof29@gmail.com>
2026-08-18 01:15:01 +0530

    Time: 36 ms (96%), Space: 19.2 MB (19.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,24 +1,40 @@
 class Solution:
     def fib(self, n: int) -> int:
 
-        memo = {0:0, 1:1}
+        #recursive O(N) appraoch using caching/memoization
 
-        def f(x):
+        # memo = {0:0, 1:1}
 
-            if x in memo:
-                return memo[x]
-            
-            memo[x] = f(x-2) + f(x-1)
-            return memo[x]
+        # def f(x):
+
+        #     if x in memo:
+        #         return memo[x]
+        #     else:
+        #         memo[x] = f(x-2) + f(x-1)
+        #         return memo[x]
         
-        return f(n)
+        # return f(n)
+
+
+        #iterative approach
+
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        
+        prev, curr = 0, 1
+
+        for i in range(2, n+1):
+            prev, curr = curr, curr+prev
+        
+        return curr
 
 
 
-        #Recusive O(2^n) soln
+        #recursive O(2^n)
         # if n == 0:
         #     return 0
-        
         # if n == 1:
         #     return 1
         
