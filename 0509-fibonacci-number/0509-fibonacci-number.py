919d2ef12c73693a22a00ab4326c96d38c619457
Zenith Kokkodan <zenithprof29@gmail.com>
2026-08-18 00:54:16 +0530

    Time: 47 ms (62.53%), Space: 19.3 MB (19.12%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,10 +1,25 @@
 class Solution:
     def fib(self, n: int) -> int:
+
+        memo = {0:0, 1:1}
+
+        def f(x):
+
+            if x in memo:
+                return memo[x]
+            
+            memo[x] = f(x-2) + f(x-1)
+            return memo[x]
         
-        if n == 0:
-            return 0
+        return f(n)
+
+
+
+        #Recusive O(2^n) soln
+        # if n == 0:
+        #     return 0
         
-        if n == 1:
-            return 1
+        # if n == 1:
+        #     return 1
         
-        return self.fib(n-2) + self.fib(n-1)
+        # return self.fib(n-2) + self.fib(n-1)
