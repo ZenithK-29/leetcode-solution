f1313fee7804dd8305f0c785e2f4bb1a4c23cc5b
Zenith Kokkodan <zenithprof29@gmail.com>
2026-08-26 11:38:43 +0530

    Time: 935 ms (17.44%), Space: 70.9 MB (12.44%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,21 +1,26 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        @cache
+        
+        memo = {}
         def dfs(left):
 
             if left == 0:
                 return 0
-            
+        
             if left < 0:
                 return float("inf")
-            
-            ans = float("inf")
-
-            for coin in coins:
-                ans = min(ans, 1+dfs(left-coin))
-            
-            return ans
         
+            if left in memo:
+                return memo[left]
+
+            ans = float("inf")
+        
+            for coin in coins:
+                ans =min (ans, 1+dfs(left-coin))
+        
+            memo[left] = ans
+            return ans
+    
         res = dfs(amount)
 
-        return -1 if res==float("inf") else res
+        return -1 if res == float("inf") else res
