ee9af2bda6741176313a9968acf48a7f7312c30c
Zenith Kokkodan <zenithprof29@gmail.com>
2026-08-30 10:48:54 +0530

    Time: 3 ms (60.66%), Space: 19.1 MB (96.57%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -2,17 +2,17 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
 
         #DP soln o(n)
-
-        cost.append(0)
         n = len(cost)
+        dp = [0] * (n+1)
 
-        for i in range(n-3, -1,-1):
 
-            cost[i] = min(cost[i]+cost[i+1], cost[i]+cost[i+2])
-        
-        return min(cost[0], cost[1])
 
-      
+        for i in range(2, n+1):
+
+            dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
+
+
+        return dp[-1]
         
 
         #Recursive O(2^n) sol
