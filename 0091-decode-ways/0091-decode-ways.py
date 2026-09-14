9034bc25060627f6fef85ab1a253367089ae77fe
Zenith Kokkodan <zenithprof29@gmail.com>
2026-09-14 22:40:43 +0530

    Time: 3 ms (24.67%), Space: 19 MB (99%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,22 +1,43 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
 
-        @cache
-        def dfs(i):
+        if s[0] == "0":
+            return 0
 
-            if i == len(s):
-                return 1
-            
-            if s[i] == "0":
-                return 0
-            
-            optionA = dfs(i+1)
+        dp = [0] * (len(s)+1)
 
-            optionB = 0
+        dp[0] = 1
+        dp[1] = 0 if s[0] == 0 else 1
 
-            if i + 2 <= len(s) and int(s[i:i+2]) in range(10, 27):
-                optionB = dfs(i+2)
+        for i in range(2, len(s) + 1):
+
+            if int(s[i-1:i]) in range(1, 10):
+                dp[i] += dp[i-1]
             
-            return (optionA + optionB)
+            if int(s[i-2:i]) in range(10, 27):
+                dp[i] += dp[i-2]
         
-        return dfs(0)
+        return dp[len(s)]
+
+
+
+        #recursive
+        # @cache
+        # def dfs(i):
+
+        #     if i == len(s):
+        #         return 1
+            
+        #     if s[i] == "0":
+        #         return 0
+            
+        #     optionA = dfs(i+1)
+
+        #     optionB = 0
+
+        #     if i + 2 <= len(s) and int(s[i:i+2]) in range(10, 27):
+        #         optionB = dfs(i+2)
+            
+        #     return (optionA + optionB)
+        
+        # return dfs(0)
