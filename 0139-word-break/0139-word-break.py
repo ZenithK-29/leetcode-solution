081783823d0b7f320dc52398b013f6fa371224ad
Zenith Kokkodan <zenithprof29@gmail.com>
2026-09-17 22:19:51 +0530

    Time: 0 ms (100%), Space: 19.2 MB (83.63%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,13 +1,14 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        maxLen = 0
+        
+        maxLen =0
         n = len(s)
-        
+
         for word in wordDict:
-            currLen = len(word)
-            maxLen = max(currLen, maxLen)
-        
-        dp = [False] * (n+1)
+            if len(word) > maxLen:
+                maxLen = len(word)
+
+        dp =[False] * (n+1)
         dp[0] = True
 
         for i in range(1, n+1):
@@ -15,7 +16,6 @@ class Solution:
             for j in range(i-1, max(-1, i-maxLen-1), -1):
 
                 if dp[j] and s[j:i] in wordDict:
-
                     dp[i] = True
                     break
         
