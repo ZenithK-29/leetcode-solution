b94aca3e297d861759869f6531546796c68d7ded
Zenith Kokkodan <zenithprof29@gmail.com>
2026-09-20 11:16:26 +0530

    Time: 419 ms (36.36%), Space: 75.8 MB (25.33%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,24 +1,55 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         
-        cache = {}
+        #Tabular DP Soln
 
-        def dfs(i, j):
+        n1, n2 = len(s), len(t)
 
-            if j == len(t):
-                return 1
-            if i == len(s):
-                return 0
-            
-            if (i, j) in cache:
-                return cache[(i, j)]
-            
-            if s[i] == t[j]:
-                cache[(i, j)] = dfs(i+1, j+1) + dfs(i+1, j)
-            
-            else:
-                cache[(i, j)] = dfs(i+1, j)
-            
-            return cache[(i, j)]
+        dp = [[0] * (n2+1) for _ in range(n1+1)]
+
+        dp[0][0] = 1
+
+        for i in range(1, n1):
+            dp[i][0] = 1
+
+        for j in range(1, n2):
+            dp[0][j] = 0
         
-        return dfs(0, 0)
+        for i in range(1, n1+1):
+
+            for j in range(1, n2+1):
+
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+                
+                else:
+                    dp[i][j] = dp[i-1][j]
+        
+        return dp[n1][n2]
+        
+        
+
+
+
+        #Memoized SOLN
+        # cache = {}
+
+        # def dfs(i, j):
+
+        #     if j == len(t):
+        #         return 1
+        #     if i == len(s):
+        #         return 0
+            
+        #     if (i, j) in cache:
+        #         return cache[(i, j)]
+            
+        #     if s[i] == t[j]:
+        #         cache[(i, j)] = dfs(i+1, j+1) + dfs(i+1, j)
+            
+        #     else:
+        #         cache[(i, j)] = dfs(i+1, j)
+            
+        #     return cache[(i, j)]
+        
+        # return dfs(0, 0)
