de97e0eb8f355ed2b2ac51d85b1ef421982c6447
Zenith Kokkodan <zenithprof29@gmail.com>
2026-09-21 21:50:20 +0530

    Time: 4 ms (5.92%), Space: 19.9 MB (8.01%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,13 +1,23 @@
-class Solution(object):
-    def uniquePaths(self, m, n):
-        """
-        :type m: int
-        :type n: int
-        :rtype: int
-        """
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        #Bottom up
+        # grid = [[0] * n for _ in range(m)]
 
+        # for i in range(m):
+
+        #     for j in range(n):
+
+        #         if i == 0 or j == 0:
+        #             grid[i][j] = 1
+                
+        #         else:
+        #             grid[i][j] = grid[i-1][j] + grid[i][j-1]
+        
+        # return grid[m-1][n-1]
+
+        #Top down memoized
         cache = {}
-
         def dfs(row, col):
 
             if row == m-1 and col == n-1:
@@ -19,16 +29,16 @@ class Solution(object):
             down = 0
             right = 0
 
-            if row >= 0 and row +1 < m:
+            if row >= 0 and row+1 < m:
                 down = dfs(row+1, col)
+    
             
             if col >= 0 and col+1 < n:
                 right = dfs(row, col+1)
             
-            cache[(row, col)] = down + right
+            cache[(row, col)] = down+right
+        
 
             return cache[(row, col)]
-
-        return dfs(0, 0)
-
         
+        return dfs(0, 0)
