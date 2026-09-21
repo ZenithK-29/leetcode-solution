9cd393909ece3e36d6cb8c5af34567178f7537d3
Zenith Kokkodan <zenithprof29@gmail.com>
2026-09-21 23:27:04 +0530

    Time: 0 ms (100%), Space: 19.3 MB (44.14%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -2,43 +2,46 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
         #Bottom up
-        # grid = [[0] * n for _ in range(m)]
+        dp =[[0] * n for _ in range(m)]
 
-        # for i in range(m):
+        for i in range(m):
+            dp[i][0] = 1
+        
+        for j in range(n):
+            dp[0][j] = 1
+        
+        for i in range(1, m):
+            for j in range(1, n):
 
-        #     for j in range(n):
-
-        #         if i == 0 or j == 0:
-        #             grid[i][j] = 1
-                
-        #         else:
-        #             grid[i][j] = grid[i-1][j] + grid[i][j-1]
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        return dp[m-1][n-1]
         
         # return grid[m-1][n-1]
 
         #Top down memoized
-        cache = {}
-        def dfs(row, col):
+        # cache = {}
+        # def dfs(row, col):
 
-            if row == m-1 and col == n-1:
-                return 1
+        #     if row == m-1 and col == n-1:
+        #         return 1
             
-            if (row, col) in cache:
-                return cache[(row, col)]
+        #     if (row, col) in cache:
+        #         return cache[(row, col)]
             
-            down = 0
-            right = 0
+        #     down = 0
+        #     right = 0
 
-            if row >= 0 and row+1 < m:
-                down = dfs(row+1, col)
+        #     if row >= 0 and row+1 < m:
+        #         down = dfs(row+1, col)
     
             
-            if col >= 0 and col+1 < n:
-                right = dfs(row, col+1)
+        #     if col >= 0 and col+1 < n:
+        #         right = dfs(row, col+1)
             
-            cache[(row, col)] = down+right
+        #     cache[(row, col)] = down+right
         
 
-            return cache[(row, col)]
+        #     return cache[(row, col)]
         
-        return dfs(0, 0)
+        # return dfs(0, 0)
