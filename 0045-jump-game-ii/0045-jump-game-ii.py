7d2291efee0dd21273fc7a3b65aef0bd2ca41831
Zenith Kokkodan <zenithprof29@gmail.com>
2026-09-23 09:37:18 +0530

    Time: 2602 ms (11.06%), Space: 42.2 MB (5.01%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -3,38 +3,38 @@ class Solution:
         
         n = len(nums)
         
-        totalJumps = 0
-        lastJumpIdx = 0
-        coverage = 0
-        destination = n-1
+        # totalJumps = 0
+        # lastJumpIdx = 0
+        # coverage = 0
+        # destination = n-1
 
-        for i in range(n-1):
+        # for i in range(n-1):
 
-            coverage = max(coverage, nums[i] + i)
+        #     coverage = max(coverage, nums[i] + i)
 
-            if i == lastJumpIdx:
-                lastJumpIdx = coverage
-                totalJumps+=1
+        #     if i == lastJumpIdx:
+        #         lastJumpIdx = coverage
+        #         totalJumps+=1
 
 
-                if coverage >= destination:
-                    return totalJumps
+        #         if coverage >= destination:
+        #             return totalJumps
         
-        return totalJumps
+        # return totalJumps
 
 
         #TOP down memoized approach
-        # @cache
-        # def dfs(i):
+        @cache
+        def dfs(i):
 
-        #     if i >= n-1:
-        #         return 0
+            if i >= n-1:
+                return 0
             
-        #     minJump = float("inf")
-        #     for j in range(i+1, i+nums[i]+1):
+            minJump = float("inf")
+            for j in range(i+1, i+nums[i]+1):
 
-        #         minJump = min(minJump, 1+dfs(j))
+                minJump = min(minJump, 1+dfs(j))
             
-        #     return minJump
+            return minJump
         
-        # return dfs(0)
+        return dfs(0)
